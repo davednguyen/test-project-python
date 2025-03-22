6e93742e0f20ab5abc82e534269407e6f97f869a
David Nguyen <davednguyen@gmail.com>
2025-03-22 16:53:39 -0700

    update test file and practice file

--- Test/test.py
+++ Test/test.py
@@ -8,3 +8,9 @@ print(response.json())
 response_user_api = requests.get("https://reqres.in/api/users?page=2")
 print(response_user_api.status_code)
 print(response_user_api.json())
+
+#Requests Library (for API Calls)
+import requests
+response = requests.get("https://api.github.com")
+print(response.status_code)
+print(response.json())
